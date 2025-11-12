b305b4c7d1875149a95e82ba600935c3b15ff295
Chaoran Lu <andyluloveusc@gmail.com>
2025-11-11 18:26:50 -0800

    update to clarify the financialIndependence definition in types

--- backend/src/core/types.ts
+++ backend/src/core/types.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod';
 
+// example for financial independent student: 
+// 1. Student who works full time, e.g. Alex, a 24 years old full-time SDE, who is pursuing MBA
+// 2. Grad Student with a stipend, e.g. Maria, a PhD student who's receiving RA stipend at UCR
+// 3. Student who used to serve the country, e.g. John served 4 years in the US Army using GI bills
 export const StudentInputSchema = z.object({
   age: z.number().int().nonnegative(),
   monthsInCA: z.number().int().min(0),
